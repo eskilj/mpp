2c0ce498d9a8a5b3aeb3fe2221f23029c9228179
Eskil J <s1245946@sms.ed.ac.uk>
2016-12-04 08:10:34 +0000

    123

---
+++ ex7/bin/precision.f90
@@ -0,0 +1,14 @@
+
+
+MODULE precision
+
+  use mpi
+
+  implicit none
+  integer, parameter :: REALNUMBER = kind(1.0e0)
+
+  integer, parameter :: MPI_REALNUMBER = MPI_REAL
+
+
+END MODULE precision
+
